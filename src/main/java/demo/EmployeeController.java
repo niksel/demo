1d7977f9c1465417e30b12b5ba438d0cd208a97d
Nikhil Selvaraj <nikhil-selvaraj@hotmail.com>
2020-08-06 20:21:23 +0530

    modified db method

--- src/main/java/demo/EmployeeController.java
+++ src/main/java/demo/EmployeeController.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.sql.DataSource;
@@ -21,22 +22,20 @@ public class EmployeeController {
 	  private DataSource dataSource;
 	
 	@GetMapping("/db")
-	public String db(Map<String, Object> model) {
+	public String db() {
 	    try (Connection connection = dataSource.getConnection()) {
 	        Statement stmt = connection.createStatement();
 	        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Employee (tick timestamp)");
 	        stmt.executeUpdate("INSERT INTO Employee VALUES (now())");
 	        ResultSet rs = stmt.executeQuery("SELECT tick FROM Employee");
 
-	        ArrayList<String> output = new ArrayList<String>();
 	        while (rs.next()) {
-	          output.add("Read from DB: " + rs.getTimestamp("tick"));
+	          System.out.println("Read from DB: " + rs.getTimestamp("tick"));
 	        }
-
-	        model.put("records", output);
 	        return "db";
+
 	      } catch (Exception e) {
-	        model.put("message", e.getMessage());
+	        
 	        return "error";
 	      }
 	    }
