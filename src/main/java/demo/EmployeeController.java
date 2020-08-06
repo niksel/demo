64a92e27711c07540a763691521c55e7659d44ec
Nikhil Selvaraj <nikhil-selvaraj@hotmail.com>
2020-08-06 17:46:31 +0530

    added procfile

---
+++ src/main/java/demo/EmployeeController.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+	@GetMapping("/employee")
+	public void getEmployee() {
+		System.out.println("Hello World!");
+	}
+}
